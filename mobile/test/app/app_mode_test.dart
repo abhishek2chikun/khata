e0c1935c04f5eb71b4999f42329899be502c7f0f
Abhishek Panigrahi <abhishek2chikun@gmail.com>
2026-05-07 20:45:52 +0530

    test: cover mobile data mode composition

--- mobile/test/app/app_mode_test.dart
+++ mobile/test/app/app_mode_test.dart
@@ -6,11 +6,23 @@ void main() {
     expect(parseDataMode(''), DataMode.api);
   });
 
+  test('parseDataMode accepts api', () {
+    expect(parseDataMode('api'), DataMode.api);
+  });
+
   test('parseDataMode accepts local', () {
     expect(parseDataMode('local'), DataMode.local);
   });
 
+  test('parseDataMode trims and normalizes case', () {
+    expect(parseDataMode(' LOCAL '), DataMode.local);
+  });
+
   test('parseDataMode rejects unknown values', () {
     expect(() => parseDataMode('server'), throwsArgumentError);
+  });
+
+  test('resolveDataMode defaults to api', () {
+    expect(resolveDataMode(), DataMode.api);
   });
 }
